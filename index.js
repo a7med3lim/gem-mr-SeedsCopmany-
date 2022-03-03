a7caf9adbb3f2e5fc74a9911291b3a0db7d9f824
unknown <a7med3lim@gmail.com>
2022-03-03 13:17:54 +0200

    eee

---
+++ index.js
@@ -0,0 +1,5 @@
+
+const btnActionSend = document.getElementById('send_message_btn');
+btnActionSend.addEventListener("click",function(){
+    alert('mail will be send');
+})
--- mail.php
+++ mail.php
@@ -1,9 +1,32 @@
-<?php
-$to = "ahmed.ali@infasme.com";
-$subject = "this is a text";
-$message = "test local host to send mail";
-$header = "From:Test@test.com";
-$time = time();
-mail($to,$subject,$message,$header);
-print "script run $time";
+<?php 
+if (isset($_POST['send_message_btn'])) {
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$phonenumber= $_POST['phonenumber'];
+	// $subject = $_POST['subject'];
+	$subject = "Contact Us";
+	$msg = $_POST['msg'];
+	// Content-Type helps email client to parse file as HTML 
+	// therefore retaining styles
+	$headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+	$message = "<html>
+	<head>
+	<title>New message from website contact form</title>
+	</head>
+	<body>
+	<h1>" . $subject . "</h1>
+	<p>The Clint Name: <strong> ". $name." </strong></p>
+	<p>The Clint Mail: <strong> ".$email." </strong></p>
+	<p>The Clint Number: <strong> ".$phonenumber." </strong></p>
+	<p>Message:</p>
+	<p> ".$msg." </p>
+	</body>
+	</html>";
+	if (mail('info@smart-extensions.com', $subject, $message, $headers)) {
+		header("Location: contactus.html?mailsend");
+	}else{
+		echo "Failed to send email. Please try again later";
+	}
+}
 ?>
